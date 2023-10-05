83703ff26fa4bd4776544c4eb6a920c487520426
Ankit <111228237+Ankit-0310@users.noreply.github.com>
2023-10-06 00:29:55 +0530

    Update team2023.data.js

--- js/team2023.data.js
+++ js/team2023.data.js
@@ -6,10 +6,10 @@ let facul = [
   {
     id: 0,
     name: "Rahul Singh",
-    avatar: "https://media.licdn.com/dms/image/C5103AQEBNohCDU1OTA/profile-displayphoto-shrink_100_100/0/1570410031016?e=1700697600&v=beta&t=5t_4PmiB1uaYMrS7hLJNF9kG0jy9Nc9PT3lY8LAXgIw",
+    avatar: "https://media.licdn.com/dms/image/C5103AQEBNohCDU1OTA/profile-displayphoto-shrink_200_200/0/1570410031016?e=1700092800&v=beta&t=aj8WFvhk3G3FovSAsPPAiiXNX0C4qkFP_t9WzDwHQsE",
     badges: `
-      <span class="red">Professor KIT</span>
-     
+      <span class="red">Professor</span>
+      <span class="green">KIT</span>
     `,
     cardBorderColor: "blue",
     // socialGithubLink: "#",
@@ -25,14 +25,45 @@ let leadMemberDetails2023 = [
     name: "Shivansh",
     avatar: "https://media.licdn.com/dms/image/D5603AQGkPiW2jNdfOQ/profile-displayphoto-shrink_200_200/0/1682364442659?e=1699488000&v=beta&t=X_XkJM1HEKLSJPL91sluKmVmUKauGPJG4NUkQsjRNOc",
     badges: `
-                    <span class="blue">GDSC LEAD</span>
-                  
+                    
+                    <span class="green">GDSC</span>
+                    <span class="yellow">Lead</span>
                 `,
     cardBorderColor: "red",
     //socialGithubLink: "#",
     socialLinkedinLink: "https://www.linkedin.com/in/shivansh-kumar-492b3021b/",
     //socialTwitterLink: "#",
-  }
+  },
+  {
+    id:1,
+    name:"Ankit",
+    avatar: "https://media.licdn.com/dms/image/D5603AQFqcXxo1hXQcg/profile-displayphoto-shrink_200_200/0/1689119799821?e=1699488000&v=beta&t=GbbspcXlB7sx7JDoyW27RwCvp6uJhNkm649zUy_0JWs",
+    badges: `
+      <span class="blue">GDSC</span>
+      <span class="red">Co-Lead</span>
+    `,
+    cardBorderColor: "blue",
+    socialGithubLink: "https://github.com/Ankit-0310",
+    socialLinkedinLink: "https://www.linkedin.com/in/ankit03102003/",
+    socialTwitterLink: "https://twitter.com/AnkitAr05704716",
+  },
+
+  {
+    id:3,
+    name:"Sweta Tiwari",
+    avatar: "https://instagram.fknu1-5.fna.fbcdn.net/v/t51.2885-19/386196133_1235850220416346_301819414388544339_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fknu1-5.fna.fbcdn.net&_nc_cat=110&_nc_ohc=52P1ZT6kFi0AX_dG4gK&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfBPlbRLOTDcnZDHEsDgAJqVodMHAGLr4HR0lxkRDs_Vlw&oe=6524B52E&_nc_sid=8b3546",
+    badges: `
+      <span class="green">Public Relation</span>
+      <span class="red">Lead</span>
+    `,
+    cardBorderColor: "blue",
+    socialGithubLink: "",
+    socialLinkedinLink: "",
+    socialTwitterLink: "",
+  },
+
+
+
 
 ];
 
@@ -43,34 +74,22 @@ let coreMembersDetails2023 = [
     name:"Anurag Pandey",
     avatar: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/anurag_cs_lt_10_1c9lX8p.jpeg",
     badges: `
-      <span class="green">web Lead</span>
-     
+      <span class="green">web</span>
+      <span class="blue">Lead</span>
       `,
     cardBorderColor: "red",
     //socialGithubLink: "#",
     socialLinkedinLink: "https://www.linkedin.com/in/anurag-pandey-a44b2b1ab/",
     socialTwitterLink: "https://twitter.com/AnuragP83549986",
   },
-  {
-    id:1,
-    name:"Ankit",
-    avatar: "https://media.licdn.com/dms/image/D5603AQFqcXxo1hXQcg/profile-displayphoto-shrink_200_200/0/1689119799821?e=1699488000&v=beta&t=GbbspcXlB7sx7JDoyW27RwCvp6uJhNkm649zUy_0JWs",
-    badges: `
-      <span class="blue">GDSC Co-Lead</span>
-     
-    `,
-    cardBorderColor: "blue",
-    socialGithubLink: "https://github.com/Ankit-0310",
-    socialLinkedinLink: "https://www.linkedin.com/in/ankit03102003/",
-    socialTwitterLink: "https://twitter.com/AnkitAr05704716",
-  },
+
   {
     id:2,
     name:"Harshita",
     avatar: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/harshita_makhija_PAOROWS.jpg",
     badges: `
-      <span class="yellow">Content Lead</span>
-      
+      <span class="yellow">Content</span>
+      <span class="red">Lead</span>
     `,
     cardBorderColor: "green",
     //socialGithubLink: "#",
@@ -82,14 +101,16 @@ let coreMembersDetails2023 = [
     name:"Aman Tripathi",
     avatar: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/aman_tripathi_nxlzgs6.jpg",
     badges: `
-      <span class="green">design Lead</span>
-      
+      <span class="green">Graphic</span>
+      <span class="red">design</span>
+      <span class="blue">Lead</span>
     `,
     cardBorderColor: "yellow",
     //socialGithubLink: "#",
     socialLinkedinLink: "https://www.linkedin.com/in/aman-tripathi-61783925a",
     socialTwitterLink: "https://twitter.com/AmanTri97133187",
   },
+
 
 ]
 
