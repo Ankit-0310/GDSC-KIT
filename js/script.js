2876c55d69b443904b9c964cdad1213fc04a047a
Ankit <111228237+Ankit-0310@users.noreply.github.com>
2023-10-06 00:29:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 // Navbar Toogle for All Pages ******************************
-
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 let menuBtn = document.querySelector("button.menu_toggle");
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
 });
 
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+if(top>= offset && top < offset + height){
+  navLinks.forEach(links => {
+    links.classList.remove('active');
+    documents.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+
+
+  });
+
+}
+
+  });
+
+}
 // Hide Navbar on Scroll ******************************
 
 let navBar = document.querySelector("header");
@@ -25,3 +45,36 @@ window.addEventListener("scroll", () => {
 
 // Add Your Scripts Here:
 // <Topic/Explain> ******************************
+
+//events
+// JavaScript to handle toggling between upcoming and past events
+document.addEventListener("DOMContentLoaded", function () {
+  const upcomingEventsLink = document.getElementById("upcoming-events-link");
+  const pastEventsLink = document.getElementById("past-events-link");
+  const upcomingEventsSection = document.getElementById("upcoming-events");
+  const pastEventsSection = document.getElementById("past-events");
+
+  // Show upcoming events by default
+  upcomingEventsLink.classList.add("active-link");
+  upcomingEventsSection.classList.add("active-section");
+
+  // Toggle to show upcoming events
+  upcomingEventsLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    upcomingEventsLink.classList.add("active-link");
+    pastEventsLink.classList.remove("active-link");
+    upcomingEventsSection.classList.add("active-section");
+    pastEventsSection.classList.remove("active-section");
+  });
+
+  // Toggle to show past events
+  pastEventsLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    upcomingEventsLink.classList.remove("active-link");
+    pastEventsLink.classList.add("active-link");
+    upcomingEventsSection.classList.remove("active-section");
+    pastEventsSection.classList.add("active-section");
+  });
+});
+
+//smooth transitions
